0ce6fb13ef54d456b57886a92004d395d33f7a0d
Lu1sMig <lmcavero.a1@gmail.com>
2025-02-13 23:39:14 -0500

    Update app.js

--- js-curso-2/app.js
+++ js-curso-2/app.js
@@ -6,3 +6,8 @@ titulo.innerHTML = 'Hora del Desafío';
 function mostarMensajeEnLaConsola() {
     console.log('El botón fue clicado!')
 }
+// 4. 
+function mostrarAlerta() {
+    let ciudad = prompt("Por favor, ingresa el nombre de una ciudad de Brasil:");
+    alert("Estuve en " + ciudad + " y me acordé de ti.");
+}
