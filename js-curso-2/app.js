6f4f3d8eaaf709c6d7335c83a13cd30d7c939cfa
Lu1sMig <lmcavero.a1@gmail.com>
2025-02-13 23:41:42 -0500

    Update app.js

--- js-curso-2/app.js
+++ js-curso-2/app.js
@@ -11,3 +11,14 @@ function mostrarAlerta() {
     let ciudad = prompt("Por favor, ingresa el nombre de una ciudad de Brasil:");
     alert("Estuve en " + ciudad + " y me acordé de ti.");
 }
+// 5 
+function mostarAlerta(){
+    alert("AMO JS");
+} 
+// 6 
+function sumaDosNumeros(){
+    let primerNumero = parseInt(prompt('Digite el primer numero'));
+    let segunNumero = parseInt(prompt('Digite el segundo numero'));
+    let resultado = primerNumero + segunNumero;
+    alert('${primerNumero} + ${segunNumero} = ${resultado}')
+}
